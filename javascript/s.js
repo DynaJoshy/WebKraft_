ac548c781afb2761fb64d9dde83d0324f5df5dc8
DEVSREE8 <129088042+DEVSREE8@users.noreply.github.com>
2023-11-18 15:36:22 +0530

    Update s.js

--- javascript/s.js
+++ javascript/s.js
@@ -51,7 +51,7 @@ const initGame = (button, clickedLetter) => {
     } else {
         // If clicked letter doesn't exist then update the wrongGuessCount and hangman image
         wrongGuessCount++;
-        hangmanImage.src = `images/hangman-${wrongGuessCount}.svg`;
+        hangmanImage.src = `Images/hangman-${wrongGuessCount}.svg`;
     }
     button.disabled = true; // Disabling the clicked button so user can't click again
     guessesText.innerText = `${wrongGuessCount} / ${maxGuesses}`;
