6332d8c968741f6f9fac8e5c6ab2abade56a705f
DEVSREE8 <129088042+DEVSREE8@users.noreply.github.com>
2023-11-18 15:03:35 +0530

    Add files via upload

---
+++ javascript/w.js
@@ -0,0 +1,74 @@
+const wordList = [
+    {
+        word: "guitar",
+        hint: "A musical instrument with strings."
+    },
+
+    {
+        word: "camera",
+        hint: "A device used to capture and record images or videos."
+    },
+    {
+        word: "diamond",
+        hint: "A precious gemstone known for its brilliance and hardness."
+    },
+    {
+        word: "adventure",
+        hint: "An exciting or daring experience."
+    },
+    {
+        word: "science",
+        hint: "The systematic study of the structure and behavior of the physical and natural world."
+    },
+
+    {
+        word: "volcano",
+        hint: "A mountain or hill with a vent through which lava, rock fragments, hot vapor, and gas are ejected."
+    },
+
+
+    {
+        word: "astronaut",
+        hint: "A person trained to travel and work in space."
+    },
+    {
+        word: "waterfall",
+        hint: "A cascade of water falling from a height."
+    },
+    {
+        word: "piano",
+        hint: "A musical instrument played by pressing keys that cause hammers to strike strings."
+    },
+    {
+        word: "oasis",
+        hint: "A fertile spot in a desert where water is found."
+    },
+    {
+        word: "photography",
+        hint: "The art, process, or practice of creating images by recording light or other electromagnetic radiation."
+    },
+    {
+        word: "river",
+        hint: "A large natural stream of water flowing in a channel to the sea, a lake, or another such stream."
+    },
+    {
+        word: "puzzle",
+        hint: "A game, toy, or problem designed to test ingenuity or knowledge."
+    },
+    {
+        word: "secret",
+        hint: "Something kept hidden or unknown to others."
+    },
+    {
+        word: "curiosity",
+        hint: "A strong desire to know or learn something."
+    },
+    {
+        word: "illusion",
+        hint: "A false perception or belief; a deceptive appearance or impression."
+    },
+    {
+        word: "brilliant",
+        hint: "Exceptionally clever, talented, or impressive."
+    },
+];
